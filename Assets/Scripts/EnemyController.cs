4fd24d1740300467cb3444db1360c9bd8ee1a747
CarlosMiraGarcia <carlosgarciamira@gmail.com>
2021-01-22 17:58:10 +0000

    Enemy follows Player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public int health = 3;
+    public float moveSpeed = 1;
+
     public GameObject explosion;
+    public float playerRange = 10f;
+    public Rigidbody2D theRB;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,18 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < playerRange)
+        {
+            Vector3 playerDirection = PlayerController.instance.transform.position - transform.position;
+
+            // Normalized will only take either x or y to multiply for the moveSpeed
+            // this way the enemy won't be moving faster when is faraway and slow when it's close to us
+            theRB.velocity = playerDirection.normalized * moveSpeed;
+        }
+        else
+        {
+            theRB.velocity = Vector2.zero;
+        }
     }
 
     public void TakeDamage()
